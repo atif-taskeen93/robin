06eed322ed32bd777d14bf89b79bcc516dbddd4c
Atif <atif.taskeen92@gmail.com>
2024-09-13 20:58:02 +0500

    add changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'uhi-new'`, () => {
+  it(`should have as title 'neptune'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('uhi-new');
+    expect(app.title).toEqual('neptune');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, uhi-new');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, neptune');
   });
 });
